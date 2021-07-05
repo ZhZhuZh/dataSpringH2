5289565ad492829699ba5e0120f5e93af4660fc7
ZhZhuZh <72465249+ZhZhuZh@users.noreply.github.com>
2021-07-05 16:40:35 +0300

    try with youtube video

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TBL_USERS (first_name, last_name, email) VALUES
+    ('Dan', 'Vega', 'vega@mail.com'),
+    ('Katty', 'Smith', 'asgk@skd.re'),
+    ('Ivan', 'Ivanov', 'ivanov@gmail.com');
